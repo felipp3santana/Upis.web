e6fd8760b82ef7007980346d0c32dc713b8b1934
Felippe de Almeida Santana <f-elipp-3@hotmail.com>
2021-08-19 22:53:16 -0300

    Contador/Autenticador Login 19-08
    Change-Id: I965c3d8cfbd74ac1b7cd997178b59c99e106b2f3

--- upis.web/src/main/java/upis/web/Primeiro.java
+++ upis.web/src/main/java/upis/web/Primeiro.java
@@ -12,18 +12,42 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ops")
 public class Primeiro extends HttpServlet{
 	
+	private int contIncorreto;
+	private int contCorreto;
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		
 		out.println("<html>");
 		out.println("<head>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("<title>Web-UPIS</title>");
 		out.println("</head>");
 		out.println("<body>");
+		out.println("<div style=\"text-align:center\">");
 		out.println("<h1>Método HTTP POST</h1>");
-		out.println("<h3>Seja bem vindo! <br /><br /> Usuário: "+req.getParameter("user") + "<br /> Senha: " + req.getParameter("password") + "</h3>");
+		
+		if (req.getParameter("user").equals("teste")) {
+			if (req.getParameter("password").equals("123")) {
+					out.println("<h3>Seja bem vindo! <br /><br /> Usuário: "+req.getParameter("user") + "<br /> Senha: " + req.getParameter("password") + "</h3>");
+					out.println("<h3>Logins corretos: "+ ++ contCorreto + "</h3>");
+			}else {
+				out.println("<h2>Senha incorreta </h2>");
+				contIncorreto++;
+				out.println("<h3>Logins incorretos: " + contIncorreto + "</h3>");
+				  }
+		}else {
+			out.println("<h2>Usuário incorreto</h2>");
+			contIncorreto++;
+			out.println("<h3>Logins incorretos: " + contIncorreto + "</h3>");
+			  }
+		
+		out.println("<input type=\"button\" value=\"Voltar\" onClick=\"history.go(-1)\"> ");
+		out.println("</div>");
 		out.println("</body>");
 		out.println("</html>");
+		
 	}
 	
 	@Override
